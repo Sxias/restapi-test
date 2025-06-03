91010f2c43d5ba8d696b93d08707d4d3ffefcf6f
sxias <a32176740@gmail.com>
2025-06-03 11:54:31 +0900

    logging comp

--- src/main/java/com/example/restapp/_core/config/FilterConfig.java
+++ src/main/java/com/example/restapp/_core/config/FilterConfig.java
@@ -1,4 +1,24 @@
 package com.example.restapp._core.config;
 
+import com.example.restapp._core.filter.LogFilter;
+import com.example.restapp.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
 public class FilterConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public FilterRegistrationBean<LogFilter> loggingFilter() {
+        FilterRegistrationBean<LogFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LogFilter());
+        registrationBean.addUrlPatterns("/*"); // 모든 요청에 적용
+        registrationBean.setOrder(1); // 필터 순서 설정
+        return registrationBean;
+    }
 }
